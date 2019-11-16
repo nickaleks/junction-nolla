a6ab7c8b92cde6fe4fd506f5b04de0e4d85443c6
Yuriy Gavrilin <snyderino@gmail.com>
2019-11-16 15:26:55 +0200

    fix nan checks

--- api.py
+++ api.py
@@ -1,6 +1,3 @@
-import requests 
-
-
 def get_product_by_ean(ean):
     root_url = "https://kesko.azure-api.net/v1/search/products"
     headers = {"Ocp-Apim-Subscription-Key": "f2d58b3f3e1a48ce8c1cef544eeef668"}
@@ -8,14 +5,19 @@ def get_product_by_ean(ean):
     query_body['filters'] = {"ean" : str(ean)}    
     r = requests.post(root_url, json=query_body, headers = headers)
 
-    product_json = r.json()
-    if product_json['totalHits'] == 0:
+    r_json = r.json()
+    
+    if r_json['totalHits'] == 0:
         return None
-
+    
+    product_json = r_json['results'][0]
+    
     if product_json['isConsumerGood']:
         return None
+    
+#     pprint.pprint(product_json, width=250)
 
-    return extract_properties(product_json['results'][0])
+    return extract_properties(product_json)
 
 attributes_to_extract = ["ENERKC", "ENERKJ", "KUITUA", "NTGEW", "PROTEG", "RASVAA", "SOKERI", "SUOLA", "HIHYDR", "ALKPI", "DVITAM"]
 def extract_properties(product_json):
@@ -23,22 +25,31 @@ def extract_properties(product_json):
     ean = product_json['ean']
     name = product_json['marketingName']['finnish']
     is_alcohol = product_json['isAlcohol']
-    brand = product_json['brand']
-    picture_url = product_json['pictureUrls'][0]['original']
+    brand = product_json.get('brand', None)
+    picture_url = product_json['pictureUrls'][0]['original'] if product_json['pictureUrls'] else None
     category_id = product_json['category']['id']
     subcategory_id = product_json['subcategory']['id']    
 
     if 'TX_RAVOMI' in product_json['attributes']:
-        n_properties = " ".join(filter(lambda x: x, [x.get('abbreviation', None) for x in product_json['attributes']['TX_RAVOMI']['value']]))
+        properties = product_json['attributes']['TX_RAVOMI']['value']
+#         print(properties)
+        if type(properties) == list:
+            n_properties = " ".join(filter(lambda x: x, [x.get('abbreviation', None) for x in properties]))
+        else:
+            n_properties = properties['abbreviation']
     else:
         n_properties = []
-    manufacturer = product_json['attributes']['ZZVENDOR']['value']['explanation']['finnish']
+        
+    if 'ZZVENDOR' in product_json['attributes']:
+        manufacturer = product_json['attributes']['ZZVENDOR']['value']['explanation']['finnish']
+    else:
+        manufacturer = None
 
     def get_attribute(attribute_name):
         if attribute_name in product_json['attributes']:
-            return product_json['attributes'][attribute_name]['value']['value'] 
-        else:
-            return None    
+            if 'value' in product_json['attributes'][attribute_name]:
+                return product_json['attributes'][attribute_name]['value']['value'] 
+        return None    
     attributes = dict(zip(attributes_to_extract, map(get_attribute, attributes_to_extract)))
     
     alcohol_content = attributes['ALKPI']
@@ -59,6 +70,8 @@ def extract_properties(product_json):
     del fetched_attributes['product_json']
     del fetched_attributes['attributes']
     del fetched_attributes['get_attribute']
+    if 'properties' in fetched_attributes:
+        del fetched_attributes['properties']
     
     return fetched_attributes
     
