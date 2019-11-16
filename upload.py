32c19404532974f289281a6db67d3bd005febda0
Nikita Alekseev <n.alekseev2104@gmail.com>
2019-11-16 12:45:53 +0300

    first commit

---
+++ upload.py
@@ -0,0 +1,73 @@
+import psycopg2
+import csv
+import itertools
+
+def grouper(iterable, n, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return itertools.zip_longest(*args, fillvalue=fillvalue)
+
+con = psycopg2.connect(database="postgres", user="postgres", password="mysecretpassword", host="40.118.124.20", port="5432")
+
+print("Database opened successfully")
+
+cur = con.cursor()
+cur.execute("SELECT * from receipt")
+rows = cur.fetchall()
+
+print(len(rows))
+
+cur = con.cursor()
+
+
+with open('/home/nikita/Junction-Kesko-Receipt-Data/Junction_data.csv') as f:
+    spamreader = csv.reader(f, delimiter=';', quotechar='"')
+    print('file read')
+    i = 0
+
+    step = 100000
+    current = 0
+    chunks = grouper(spamreader, step)
+    for chunk in chunks:
+        if i == 0:
+            i += 1
+            continue
+        rows = ""
+        for row in chunk:
+            single_row = f"""({row[0]}, {row[1]}, '{row[2]}', '{row[3]}', '{row[4]}', '{row[5]}', '{row[6]}', {row[7]}, '{row[8]}', '{row[9]}')"""
+            rows += single_row
+            rows += ','
+        rows = rows[:-1]
+        query = f"""
+        INSERT INTO receipt (AreaId,Receipt,TransactionDate,BeginHour,EAN,Quantity,PersonAgeGrp,KCustomer,QualClass,EasyClass) VALUES {rows};"""
+        cur.execute(query)
+        current += step
+        print(current)
+    con.commit()
+    # for row in spamreader:
+    #     if i == 0:
+    #         i += 1
+            
+    #         continue
+    #     single_row = f"""(
+    #         {row[0]}, 
+    #         {row[1]}, 
+    #         '{row[2]}', 
+    #         '{row[3]}', 
+    #         '{row[4]}', 
+    #         '{row[5]}', 
+    #         '{row[6]}', 
+    #         {row[7]}, 
+    #         '{row[8]}', 
+    #         '{row[9]}'
+    #     )"""
+        
+    #     query = f"""
+    #     INSERT INTO receipt (AreaId,Receipt,TransactionDate,BeginHour,EAN,Quantity,PersonAgeGrp,KCustomer,QualClass,EasyClass) VALUES {single_row}"""
+    #     print(i)
+    #     i += 1
+    #     
+        
+
+
